b2b37a11135fd5dfbdae9fb9e1388ff9e3f51c21
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-07-28 17:51:33 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Button from '@material-ui/core/Button'
+import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+
+
+
+function App() {
+  return (
+  <div>
+    jasjaksas
+    <Button>ESTE ES UN BOTON</Button>
+    <Button variant='contained' color="primary" href="https://getbootstrap.com/docs/5.0/helpers/position/">
+      Boton href
+    </Button>
+    <DeleteIcon />
+  </div>
+  );
+}
+
+export default App;
